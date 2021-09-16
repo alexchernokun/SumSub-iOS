e04434dcaa4903e720fc6fef40346b6437ad2707
Yuri Drigin <yuri.drigin@icloud.com>
2021-09-17 00:36:36 +0300

    Remove unnessesary ZoomSDK

--- Package.swift
+++ Package.swift
@@ -12,16 +12,13 @@ let package = Package(
             name: "IdensicMobileSDK",
             targets: ["IdensicMobileSDK"]),
     ],
+    dependencies: [
+    ],
     targets: [
         .binaryTarget(
             name: "IdensicMobileSDK",
             url: "https://github.com/SumSubstance/IdensicMobileSDK-iOS-Release/raw/master/1.18.0/IdensicMobileSDK-1.18.0.zip",
             checksum: "da58ca91e32a0e99bdd1391b5069465d8b9929f7b233808fd5784403e8c0dfff"
-        ),
-        .binaryTarget(
-            name: "ZoomAuthentication",
-            url: "https://github.com/SumSubstance/IdensicMobileSDK-iOS-Release/raw/master/Deps/ZoomAuthSDK-8.12.1.zip",
-            checksum: "81f251b5baf2bb1c461b3c12e4030df0ef81aa41c9bdb450d6571eda72f7057e"
         )
     ]
 )
